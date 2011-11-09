2f8c06a1cf119396d98c44a4f1726180402ec013
Squeeks <squeek@cpan.org>
2011-11-09 22:51:17 +0000

    Import of SQUEEK/App-CPAN-MetaDB-0.01 from CPAN.
    gitpan-cpan-distribution: App-CPAN-MetaDB
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SQUEEK/App-CPAN-MetaDB-0.01.tar.gz
    gitpan-cpan-author:       SQUEEK
    gitpan-cpan-maturity:     released

---
+++ lib/App/CPAN/MetaDB/Redis.pm
@@ -0,0 +1,61 @@
+package App::CPAN::MetaDB::Redis;
+
+=head1 NAME
+
+App::CPAN::MetaDB::Redis
+
+=head1 SYNOPSIS
+
+This provides the interface to store and retrieve data from the specified
+L<Redis> database.
+
+=cut
+
+use strict;
+use warnings;
+
+use Redis;
+=head1 CONSTRUCTION
+
+=head2 new
+
+Requires you supply a hashref with the "server" key pointing to the appropriate
+server:port. You can also add in other keys that L<Redis>->new will accept.
+
+=cut
+sub new {
+
+    my($class, %opts) = @_;
+
+    return bless {
+        db => Redis->new(server => $opts{server})
+    }, $class;
+
+}
+
+sub _find_package {
+    my($self, $package_name) = @_;
+
+    return $self->{db}->get($package_name);
+}
+
+sub _update_package {
+
+    my($self, %data) = @_;
+
+    $self->{db}->set( 
+        $data{name} => 
+            sprintf "---\ndistfile: %s\nversion: %s\n",
+                $data{path},
+                $data{version}
+    );
+
+}
+
+=head1 SEE ALSO
+
+L<App::CPAN::MetaDB>
+
+=cut
+
+1;
